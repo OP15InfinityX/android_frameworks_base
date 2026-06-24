f6e4ba1c8ccb527732c66a2e3c17e856c32d4eaf
Jonas Salo <salomannen1234@gmail.com>
2026-06-24 15:46:41 +0200

    frameworks: sync OP15 app lock and refresh fixes

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -6265,6 +6265,11 @@ public abstract class Context {
     public static final String AX_SANDBOX_SERVICE = "ax_sandbox";
 
     /**
+     * @hide
+     */
+    public static final String APP_LOCK_SERVICE = "app_lock";
+
+    /**
      * Use with {@link #getSystemService(String)} to retrieve a {@link android.app.role.RoleManager}
      * for managing roles.
      *
